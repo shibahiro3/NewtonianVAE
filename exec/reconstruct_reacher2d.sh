0761329953908aacbb094cfb130f7af64777c468
Your Name <you@example.com>
2022-11-10 16:11:35 +0900

    Revised requirements.txt

--- exec/reconstruct_reacher2d.sh
+++ exec/reconstruct_reacher2d.sh
@@ -1,7 +1,7 @@
 cd ../
 python nvae/reconstruct.py \
 	--cf-eval reacher2d/cf/params_eval.json5 \
-	--path-model reacher2d/saves \
+	--path-model reacher2d/saves_trained \
 	--episodes 10 \
 	$@
 	# --path-model reacher2d/saves_trained \
