56f97901b13387c5916471da1dff547f10dffbcc
Your Name <you@example.com>
2022-11-10 21:05:00 +0900

    revised .gitignore

---
+++ mypython/ai/torchprob/__init__.py
@@ -0,0 +1,9 @@
+"""
+Ref:
+    https://github.com/masa-su/pixyz
+    https://github.com/pyro-ppl/pyro
+"""
+
+from .distributions.bernoulli import Bernoulli
+from .distributions.normal import Distribution, Normal, Normal01
+from .functions import KLdiv, log
---
+++ mypython/ai/torchprob/distributions/__init__.py
@@ -0,0 +1,2 @@
+from .bernoulli import Bernoulli
+from .normal import Normal
