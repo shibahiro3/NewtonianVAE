b436347cc224ae138c8f710d3fbe407a0ddc5af3
Your Name <you@example.com>
2022-12-17 23:15:51 +0900

    Add third LICENSE

--- source/third/__init__.py
+++ source/third/__init__.py
@@ -2,4 +2,11 @@
 
 Check: # Changed/added by Sugar
 
+
+LICENSE
+    dm_control: Apache License 2.0
+    https://github.com/deepmind/dm_control/blob/main/LICENSE
+
+    pyjson5: Apache License 2.0
+    https://github.com/dpranke/pyjson5/blob/main/LICENSE
 """
