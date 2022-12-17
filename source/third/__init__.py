ab01d5b16c75b1ff73906d540705ef32e2ee6aa3
Your Name <you@example.com>
2022-12-18 07:03:21 +0900

    add link

--- source/third/__init__.py
+++ source/third/__init__.py
@@ -5,8 +5,10 @@ Check: # Changed/added by Sugar
 
 LICENSE
     dm_control: Apache License 2.0
+    https://pypi.org/project/dm-control/
     https://github.com/deepmind/dm_control/blob/main/LICENSE
 
     pyjson5: Apache License 2.0
+    https://pypi.org/project/json5/
     https://github.com/dpranke/pyjson5/blob/main/LICENSE
 """
